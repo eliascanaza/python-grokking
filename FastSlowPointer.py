e579ad33b5b5c44c4726c653b9c59a579abea10d
eliascanaza <sistemc44@gmail.com>
2025-03-08 10:59:56 -0600

    happy number algorithm

--- FastSlowPointer.py
+++ FastSlowPointer.py
@@ -1,27 +1,25 @@
 class FastSlowPointer:
     def happyNumber(_self, number):
-        sumNumbers = set()
-        sumNumbers.add(number)
-        
-        while len(sumNumbers) > 0:
-            sum = 0 
+        sum = number
+        numList = set()
+        numList.add(number)
+
+        while sum != 1:
+            sum = 0
+            
             while number > 0:
-                rest = (int) (number % 10)
-                number = number // 10
-                sum += rest * rest
-                
+                digit = number % 10
+                number //= 10
+                sum += digit * digit
+            
+            if sum in numList: 
+                return False
+            
+            numList.add(sum)
             number = sum
 
-            if sum == 1:
-                return True
-            elif number not in sumNumbers:
-                sumNumbers.add(sum)
-            else:
-                break
-            
-        print(sumNumbers)
-        return False
+        return True
     
 fastSlowPointer = FastSlowPointer()
 
-print(fastSlowPointer.happyNumber(26))
+print(fastSlowPointer.happyNumber(40))
