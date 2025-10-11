44463c6f6327f85de9179c33287768acd077165e
eliascanaza <sistemc44@gmail.com>
2025-10-11 14:42:09 -0500

    longest subarray

--- SlidingWindowPattern/Main.py
+++ SlidingWindowPattern/Main.py
@@ -1,9 +1,10 @@
 from SlidingWindowPattern import SlidingWindow
 
 slidingWindow = SlidingWindow()
-case1 = [4,2,3,1,5,7,6,8]
-case2 = [2,3]
+case1 = [1,2,3,4,5,2,1,3]
+case2 = [1,2,1,2,1,4,2,1,2]
 print("Greatest Sum: ", slidingWindow.fixed(case1, 3))
 print(slidingWindow.fixed(case2, 3))
 
 print("All sum window: ",slidingWindow.allWindow(case1, 4))
+print("Longest sum array: ",slidingWindow.longest_subarray(case2, 7))
--- SlidingWindowPattern/SlidingWindowPattern.py
+++ SlidingWindowPattern/SlidingWindowPattern.py
@@ -14,11 +14,32 @@ class SlidingWindow:
     
     def allWindow(self, arr, k):
         res = []
+
+        #first sum window elements
         windowSum = sum(arr[:k])
         res.append(windowSum)
 
+        #iterate adding and removing elements for the window
         for i in range(k, len(arr)):
             windowSum += arr[i] - arr[i - k]
             res.append(windowSum)
 
         return res
+
+    def longest_subarray(self, arr, k):
+        left, sum = 0, 0
+        longest_subarray = 0
+
+        #iterate all elements
+        for right in range(len(arr)):
+            sum += arr[right]
+
+            #if does not meet the condition
+            while sum > k:
+                sum -= arr[left]
+                left += 1
+
+            #getting the longest sub array
+            longest_subarray = max (longest_subarray, right - left + 1)
+
+        return longest_subarray
