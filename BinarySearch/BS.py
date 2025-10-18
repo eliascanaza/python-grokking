516b1067feab436ee8b23e3f26930f0a3c244738
eliascanaza <sistemc44@gmail.com>
2025-10-18 15:56:49 -0500

    find in rotated array

--- BinarySearch/BS.py
+++ BinarySearch/BS.py
@@ -13,3 +13,24 @@ class BinarySearchPattern:
                 start = middle + 1
         
         return -1
+
+    def findInRotatedArray(self, nums, target):
+        s, e = 0, len(nums) - 1
+
+        while s <= e:
+            m = (s + e) // 2
+
+            if nums[m] == target:
+                return m
+            
+            if nums[s] <= nums[m]:
+                if nums[s] <= target and target < nums[m]:
+                    e = m - 1
+                else:
+                    s = m + 1
+            else:
+                if nums[m] < target and target <= nums[e]:
+                    s = m + 1
+                else:
+                    e = m - 1
+        return -1
--- BinarySearch/Main.py
+++ BinarySearch/Main.py
@@ -10,12 +10,18 @@ target2 = 61
 list3 = [11,22,33,44,55,66,77]
 target3 = 33
 
-print("Binary Search:")
-print(list1, "target: ", target1)
-print("Index: ",bs.findElement(list1, target1))
-print()
-print(list2, "target: ", target2)
-print("Index: ",bs.findElement(list2, target2))
-print()
-print(list3, "target: ", target3)
-print("Index: ",bs.findElement(list3, target3))
+# print("Binary Search:")
+# print(list1, "target: ", target1)
+# print("Index: ",bs.findElement(list1, target1))
+# print()
+# print(list2, "target: ", target2)
+# print("Index: ",bs.findElement(list2, target2))
+# print()
+# print(list3, "target: ", target3)
+# print("Index: ",bs.findElement(list3, target3))
+
+list4 = [7,8,9,1,2,3,4,5,6]
+target4 = 3
+print("Binary Reverted Search:")
+print(list4, "target: ", target4)
+print("Index: ",bs.findInRotatedArray(list4, target4))
